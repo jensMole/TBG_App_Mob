a8e0feabbd48af1390893160de6335d95af6ca3a
jens molenaers <jensmolenaers@hotmail.com>
2017-11-19 10:03:10 +0100

    update project

---
+++ TBG/app/src/main/java/com/example/jensie/tbg/ResetPasswordActivity.java
@@ -0,0 +1,77 @@
+package com.example.jensie.tbg;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by jensie on 16-11-2017.
+ * Als een gebruiker zijn wachtwoord wil resetten.
+ */
+
+public class ResetPasswordActivity extends AppCompatActivity{
+
+    private EditText inputEmail;
+    private Button btnReset, btnBack;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        btnReset = (Button) findViewById(R.id.btn_reset_password);
+        btnBack = (Button) findViewById(R.id.btn_back);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        auth = FirebaseAuth.getInstance();
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Vul je geregistreerde email in!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ResetPasswordActivity.this, "We hebben je een mail gestuurd met instructies om je wachtwoord te resetten! Let op de mail kan in Spam staan.", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(ResetPasswordActivity.this, "Gefaald voor het zenden van de reset mail!", Toast.LENGTH_SHORT).show();
+                                }
+
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
+    }
+
+}
